5f81f20b65397b1817cc7771415c031b923388c9
Lorenzo Fontana <fontanalorenzo@me.com>
2016-08-11 16:31:19 +0200

    rebuilding site Thu Aug 11 16:31:19 CEST 2016

--- deploy.sh
+++ deploy.sh
@@ -17,4 +17,5 @@ git commit -m "$msg"
 
 # Push source and build repos.
 git push origin master
+git push origin :gh-pages
 git subtree push --prefix=public git@github.com:fntlnz/blog.fntlnz.wtf.git gh-pages
