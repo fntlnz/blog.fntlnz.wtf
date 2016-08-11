dd755ace06247fe1c0076622a81f0e446ada28bf
Lorenzo Fontana <fontanalorenzo@me.com>
2016-08-11 16:07:19 +0200

    Why containers post

--- deploy.sh
+++ deploy.sh
@@ -17,4 +17,4 @@ git commit -m "$msg"
 
 # Push source and build repos.
 git push origin master
-git subtree push --force --prefix=public git@github.com:fntlnz/blog.fntlnz.wtf.git gh-pages
+git subtree push --prefix=public git@github.com:fntlnz/blog.fntlnz.wtf.git gh-pages
